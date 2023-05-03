599d31eb7dfd284bc9a3c2dea9492e6ba7e6a5f1
BRANTEC11 <brandon11leung@gmail.com>
2023-05-03 19:51:46 -0400

    Added proper image links to seed.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -8,12 +8,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_CLOUD_KEY_SECRET,
 });
 
-// const SS = ["./images/Pokemon SoulSilver/1 - sjYs7He.jpg",
-//             "./images/Pokemon SoulSilver/2 - xHrMU9U.jpg",
-//             "./images/Pokemon SoulSilver/3 - K2r6Qnc.jpg",
-//             "./images/Pokemon SoulSilver/4 - k2JHBzZ.jpg",
-//             "./images/Pokemon SoulSilver/5 - 0Tw1cei.jpg"
-//             ]
+
 
 export const uploadImage = async (imageArr) => {
     let urlArr = [];
@@ -29,4 +24,12 @@ export const uploadImage = async (imageArr) => {
     }
 } 
 
-//console.log(uploadImage(SS))
+// const SS = ["./images/Pokemon SoulSilver/1 - sjYs7He.jpg",
+//             "./images/Pokemon SoulSilver/2 - xHrMU9U.jpg",
+//             "./images/Pokemon SoulSilver/3 - K2r6Qnc.jpg",
+//             "./images/Pokemon SoulSilver/4 - k2JHBzZ.jpg",
+//             "./images/Pokemon SoulSilver/5 - 0Tw1cei.jpg"
+//             ]
+
+const RT = ["./images/Rhythm Thief/s-l600.jpg"]
+console.log(uploadImage(RT))
