cf00f3b45d88000fabc96fd0b7395b5c685dac4e
lkbnch <lkbnch@gmail.com>
2023-05-04 17:34:33 -0400

    Client side transactions

--- public/js/transactionCheck.js
+++ public/js/transactionCheck.js
@@ -1,4 +1,5 @@
 let upper = /[A-Z]/;
+let lower = /[a-z]/;
 let nums = /\d/;
 let specials = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 let citySpecials = /[&,:;()[\]{}+#$%@!?"•°]/;
@@ -167,6 +168,128 @@ if (form) {
             error = 'Error: Select a state'
             errorState.hidden = false;
             errorState.innerHTML = error;
+        }
+    })
+}
+
+let form2 = document.getElementById('payment-form');
+
+let errorCardholder = document.getElementById('errorCardholder');
+let errorCardnumber = document.getElementById('errorCardnumber');
+let errorExpiration = document.getElementById('errorExpiration');
+let errorCvv = document.getElementById('errorCvv');
+
+if (form2) {
+    form2.addEventListener('submit', (event) => {
+        let cardholder = document.getElementById('cardholder');
+        let cardnumber = document.getElementById('cardnumber');
+        let expiration = document.getElementById('expiration');
+        let cvv = document.getElementById('cvv');
+        let error;
+
+        errorCardholder.hidden = true;
+        errorCardnumber.hidden = true;
+        errorExpiration.hidden = true;
+        errorCvv.hidden = true;
+
+        cardholder = cardholder.value.trim();
+        cardnumber = cardnumber.value.trim();
+        expiration = expiration.value.trim();
+        cvv = cvv.value.trim();
+
+        event.preventDefault();
+
+        //Check Name
+        if ((!(cardholder.replace(/\s/g, '').length)) || (cardholder.length < 2) || (cardholder.length > 25)) {
+            error = 'Error: Name either an empty space(s) or does not meet the length specifications'
+            errorCardholder.hidden = false;
+            errorCardholder.innerHTML = error;
+        }
+        let findNum = cardholder.match(/[^a-zA-Z\s]+/g);
+        if (findNum !== null) {
+            error = 'Error: Name contains a number or special character'
+            errorCardholder.hidden = false;
+            errorCardholder.innerHTML = error;
+        }
+
+        //Check cardnumber
+        if (cardnumber.match(upper) !== null || cardnumber.match(lower) !== null || cardnumber.match(specials)) {
+            error = 'Error: Invalid cardnumber'
+            errorCardnumber.hidden = false;
+            errorCardnumber.innerHTML = error;
+        }
+        if (cardnumber.length < 13 || cardnumber.length > 16) {
+            error = 'Error: Invalid cardnumber'
+            errorCardnumber.hidden = false;
+            errorCardnumber.innerHTML = error;
+        }
+
+        //Check cvv
+        if (cvv.match(upper) !== null || cvv.match(lower) !== null || cvv.match(specials)) {
+            error = 'Error: Invalid cvv'
+            errorCardnumber.hidden = false;
+            errorCardnumber.innerHTML = error;
+        }
+        if (cvv.length < 3 || cvv.length > 4) {
+            error = 'Error: Invalid cvv'
+            errorCardnumber.hidden = false;
+            errorCardnumber.innerHTML = error;
+        }
+
+        //Check Expiration
+        let beg = expiration.slice(0,2);
+        let mid = expiration.slice(2,3);
+        let end = expiration.slice(3);
+        if (beg.match(nums) === null) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
+        }
+        if (mid.match(/[/]/) === null) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
+        }
+        if (end.match(nums) === null) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
+        }
+        if (end.length !== 2) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
+        }
+            //Check the year
+        let validYears = [];
+        let validMonths = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear(); 
+        let stryear = currentYear.toString();
+        let stryear3 = stryear.slice(2);
+        stryear = parseInt(stryear3);
+        for (let i = stryear; i < stryear+6; i++) {
+            validYears.push(i);
+        }
+        const stringYear = validYears.map(String);
+        if (stringYear.includes(end) === false) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
+        }
+            //Check the month
+        if (validMonths.includes(beg) === false) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
+        }
+        const checkMnth = parseInt(beg);
+        let currentMonth = currentDate.getMonth();
+        currentMonth = currentMonth + 1;
+        if (checkMnth < currentMonth && end === stryear3) {
+            error = 'Error: Invalid expiration date'
+            errorExpiration.hidden = false;
+            errorExpiration.innerHTML = error;
         }
     })
 }
