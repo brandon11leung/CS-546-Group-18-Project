eb896ad887542fa7f4d79773e8680fe49211131b
lkbnch <lkbnch@gmail.com>
2023-05-09 18:54:25 -0400

    logged out bug fix

--- routes/main.js
+++ routes/main.js
@@ -66,11 +66,14 @@ router.route('/sellinglistings/:id').get(async (req, res) => {
             if(List.trades.length === 1 && List.trades[0] === ''){
                 tradeCheck = false;
             }
-
-        if (username === req.session.user.username) {
-            res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Buy Now", username: username, check: true, tradeCheck: tradeCheck, ownPoster: false});
+        if (req.session.user) {
+            if (username === req.session.user.username) {
+                res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Buy Now", username: username, check: true, tradeCheck: tradeCheck, ownPoster: false});
+            } else {
+                res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Buy Now", username: username, check: true, tradeCheck: tradeCheck, ownPoster: true});
+            }
         } else {
-            res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Buy Now", username: username, check: true, tradeCheck: tradeCheck, ownPoster: true});
+            res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Buy Now", username: username, check: true, tradeCheck: tradeCheck, ownPoster: false});
         }
         } catch (e) {
         res.status(500).json({error: e});
@@ -220,10 +223,14 @@ router.route('/buyinglistings/:id').get(async (req, res) => {
             if(List.trades.length === 1 && List.trades[0] === ''){
                 tradeCheck = false;
             }
-            if (username === req.session.user.username) {
-                res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Offer Trade", username: username, check: false, tradeCheck: tradeCheck, ownPoster: false});
+            if (req.session.user) {
+                if (username === req.session.user.username) {
+                    res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Offer Trade", username: username, check: false, tradeCheck: tradeCheck, ownPoster: false});
+                } else {
+                    res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Offer Trade", username: username, check: false, tradeCheck: tradeCheck, ownPoster: true});
+                }
             } else {
-                res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Offer Trade", username: username, check: false, tradeCheck: tradeCheck, ownPoster: true});
+                res.render('listingsById', {title: List.title,listings: List, chart: priceChartData, type: "Offer Trade", username: username, check: false, tradeCheck: tradeCheck, ownPoster: false});
             }
             } catch (e) {
             res.status(500).json({error: e});
